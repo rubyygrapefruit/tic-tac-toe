2a7b7fb88d50a5c9f5351f1ecb9317fe42eaf973
Ruby Lee <rubymleee@gmail.com>
2019-04-11 00:54:24 -0700

    User can click on squares to display X's and O's on board + shows status of who is playing next

--- my-app/src/Components/Board.js
+++ my-app/src/Components/Board.js
@@ -7,12 +7,16 @@ class Board extends Component {
 		super(props);
 		this.state = {
 			board: Array(9).fill(null),
+			value: null,
 			xIsNext: true,
 		}
 		this.createNewBoard = this.createNewBoard.bind(this);
+		this.renderSquare = this.renderSquare.bind(this);
+		this.handleClick = this.handleClick.bind(this);
 	}
 
 	createNewBoard(boardSize) {
+		// I made this particular function so that the board size can change dynamically
 		let squares = [];
 		for (let i = 0; i < boardSize; i++) {
 			let row = [];
@@ -29,15 +33,31 @@ class Board extends Component {
 		return (
 			<Square
 				key={i}
-				value={ board[i] }
+				value={board[i]}
+				onHandleClick={()=> this.handleClick(i)}
 				/>
 		)
 	}
 
+	handleClick(i) {
+		const { board, xIsNext } = this.state;
+		const boardCopy = board.slice();
+		boardCopy[i] = xIsNext ? 'X' : 'O';
+		this.setState({
+			board: boardCopy,
+			xIsNext: !xIsNext,
+		})
+	}
+
 
   render() {
+		const { xIsNext } = this.state;
+ 		const status = `Next player is ${xIsNext ? 'X': 'O'}`
     return (
-      <div>{ this.createNewBoard(3) }</div>
+			<div>
+				{status}
+      	<div>{this.createNewBoard(3)}</div>
+			</div>
     );
   }
 }
--- my-app/src/Components/Square.js
+++ my-app/src/Components/Square.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Square({ value }) {
+function Square({ value, onHandleClick }) {
 	return(
-		<button className='square'> { value } </button>
+		<button className={'square ' + value} onClick={ onHandleClick }>{value}</button>
 	);
 }
 
